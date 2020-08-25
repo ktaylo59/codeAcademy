e9c9b3f3afa1cfbcdae7ef28f8a372b84dbf38a8
ktaylo59 <67480468+ktaylo59@users.noreply.github.com>
2020-08-24 23:22:06 -0500

    Add files via upload

---
+++ UnitConversion/src/task4.java
@@ -0,0 +1,9 @@
+
+public class task4 {
+	static double inch = 1000.0;
+	static double meters = .0254 * inch;
+	
+	public static void main(String[] args) {
+		System.out.println(inch + " inch is " + meters + " meters");
+	}
+}
